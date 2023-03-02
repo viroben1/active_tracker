872b502c87f8b25b50d2c2e02a826308f5e6dd08
benjamin farmer <viroben2001@gmail.com>
2023-03-02 14:41:04 -0800

    created login and regiter funconality

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,18 @@
 <template>
-  <header></header>
+  <header class= " bg-at-light-green text-white">
+    <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
+    <div class="flex items-center gap-x-4">
+        <img class="w-14" src="../assets/images/dumbbell-light.png" alt="" />
+        <h1 class="text-lg">Active Tracker</h1>
+      </div>
+       <ul class="flex flex-1 justify-end gap-x-10">
+       </ul>
+        <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
+        <router-link class="cursor-pointer" :to="{ name: '' }">Create</router-link>
+        <router-link class="cursor-pointer" :to="{ name: 'Login' }">Login</router-link>
+        <li class ="cursor-pointer">Logout</li>
+  </nav>
+  </header>
 </template>
 
 <script>
